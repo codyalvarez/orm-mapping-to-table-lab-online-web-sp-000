2896aaee3fb45d0590ce44cff5226f655d1dba25
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-23 02:18:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,9 @@
 class Student
+  def initialize(name)
+    @name = name
+    @grade = grade
+    @id = nil
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
